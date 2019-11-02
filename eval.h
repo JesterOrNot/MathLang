1151384faee494ecd6afee3a072715657a0f7da7
Sean Hellum <seanhellum45@gmail.com>
2019-11-02 16:35:58 +0000

    translated Python to C++ for speed

---
+++ eval.h
@@ -0,0 +1,73 @@
+#include <string>
+using namespace std;
+double eval(string expr)
+{
+    string xxx; // Get Rid of Spaces
+    for (int i = 0; i < expr.length(); i++)
+    {
+        if (expr[i] != ' ')
+        {
+            xxx += expr[i];
+        }
+    }
+
+    string tok = ""; // Do parantheses first
+    for (int i = 0; i < xxx.length(); i++)
+    {
+        if (xxx[i] == '(')
+        {
+            int iter = 1;
+            string token;
+            i++;
+            while (true)
+            {
+                if (xxx[i] == '(')
+                {
+                    iter++;
+                } else if (xxx[i] == ')')
+                {
+                    iter--;
+                    if (iter == 0)
+                    {
+                        i++;
+                        break;
+                    }
+                }
+                token += xxx[i];
+                i++;
+            }
+            //cout << "(" << token << ")" << " == " << to_string(eval(token)) <<  endl;
+            tok += to_string(eval(token));
+        }
+        tok += xxx[i];
+    }
+
+    for (int i = 0; i < tok.length(); i++)
+    {
+        if (tok[i] == '+')
+        {
+            //cout << tok.substr(0, i) + " + " +  tok.substr(i+1, tok.length()-i-1) << " == " << eval(tok.substr(0, i)) + eval(tok.substr(i+1, tok.length()-i-1)) << endl;
+            return eval(tok.substr(0, i)) + eval(tok.substr(i+1, tok.length()-i-1));
+        } else if (tok[i] == '-')
+        {
+            //cout << tok.substr(0, i) + " - " +  tok.substr(i+1, tok.length()-i-1) << " == " << eval(tok.substr(0, i)) - eval(tok.substr(i+1, tok.length()-i-1)) << endl;
+            return eval(tok.substr(0, i)) - eval(tok.substr(i+1, tok.length()-i-1));
+        }
+    }
+
+    for (int i = 0; i < tok.length(); i++)
+    {
+        if (tok[i] == '*')
+        {
+            //cout << tok.substr(0, i) + " * " +  tok.substr(i+1, tok.length()-i-1) << " == " << eval(tok.substr(0, i)) * eval(tok.substr(i+1, tok.length()-i-1)) << endl;
+            return eval(tok.substr(0, i)) * eval(tok.substr(i+1, tok.length()-i-1));
+        } else if (tok[i] == '/')
+        {
+            //cout << tok.substr(0, i) + " / " +  tok.substr(i+1, tok.length()-i-1) << " == " << eval(tok.substr(0, i)) / eval(tok.substr(i+1, tok.length()-i-1)) << endl;
+            return eval(tok.substr(0, i)) / eval(tok.substr(i+1, tok.length()-i-1));
+        }
+    }
+
+    //cout << stod(tok.c_str()) << endl;
+    return stod(tok.c_str()); // Return the value...
+}
