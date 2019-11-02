974d3a5b50de05a669f17cd78c1ba45c4505a24d
Sean Hellum <seanhellum45@gmail.com>
2019-11-02 22:33:10 +0000

    add + operator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate regex;
+use regex::Regex;
 use std::env;
 use std::io::{BufReader,BufRead};
 use std::fs::File;
@@ -18,6 +20,12 @@ fn main(){
         for line in lines {
             if &line[..4] == "sqrt" {
                 println!("{}",f64::sqrt(line[5..].parse::<f64>().unwrap()));
+            }
+            else if line.contains("+") {
+                let re = Regex::new(r"(\d*) \+ (\d*)").unwrap();
+                for cap in re.captures_iter(&line) {
+                    println!("{}", &cap[1].parse::<f64>().unwrap() + &cap[2].parse::<f64>().unwrap());
+                }
             }
         }
     } else {
