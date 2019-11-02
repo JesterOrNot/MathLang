33b04c3c9cd6ad19d27b378ceae38ad8f6204245
Sean Hellum <seanhellum45@gmail.com>
2019-11-02 19:47:08 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::env;
+use std::io::{BufReader,BufRead};
+use std::fs::File;
 
 fn main(){
     let args: Vec<_> = env::args().collect();
-    println!("{:?}", args);
+    if args.len() == 2 {
+        let file = File::open(&args[1]).unwrap();
+        for line in BufReader::new(file).lines() {
+            println!("{}", line.unwrap());
+        }
+    } else {
+        println!("usage: mathlang [FILE_NAME]");
+    }
 }
