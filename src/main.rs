794d5a327a92d9585c3204904b12fa0e071b20ce
Sean Hellum <seanhellum45@gmail.com>
2019-11-03 02:43:50 +0000

    format

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,14 @@ fn main() {
             if &line[..4] == "sqrt" {
                 println!("{}", f64::sqrt(line[5..].parse::<f64>().unwrap()));
             } else if &line[..3] == "log" {
-                let lists:Vec<_> = line.split(" ").collect();
-                println!("{}", lists[1].parse::<f64>().unwrap().log(lists[2].parse::<f64>().unwrap()));
+                let lists: Vec<_> = line.split(" ").collect();
+                println!(
+                    "{}",
+                    lists[1]
+                        .parse::<f64>()
+                        .unwrap()
+                        .log(lists[2].parse::<f64>().unwrap())
+                );
             } else if line.contains("+") {
                 let re = Regex::new(r"(\d*) \+ (\d*)").unwrap();
                 for cap in re.captures_iter(&line) {
