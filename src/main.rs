081a9bc1751ff0e8a057c448ca3f5b2584d3d30a
Sean Hellum <seanhellum45@gmail.com>
2019-11-03 04:22:25 +0000

    atan

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,9 @@ fn main() {
             } else if &line[..4] == "asin" {
                 let lists: Vec<_> = line.split(" ").collect();
                 println!("{}", lists[1].parse::<f64>().unwrap().sin().asin());
+            } else if &line[..4] == "atan" {
+                let lists: Vec<_> = line.split(" ").collect();
+                println!("{}", lists[1].parse::<f64>().unwrap().tan().atan());
             } else if line.contains("+") {
                 let re = Regex::new(r"(\d*) \+ (\d*)").unwrap();
                 for cap in re.captures_iter(&line) {
