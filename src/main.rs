c33c21368fc0ffce4cb4ec63efce2c06b3062e54
Sean Hellum <seanhellum45@gmail.com>
2019-11-03 04:16:10 +0000

    add cbrt function

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,9 @@ fn main() {
             } else if &line[..3] == "tan" {
                 let lists: Vec<_> = line.split(" ").collect();
                 println!("{}", lists[1].parse::<f64>().unwrap().tan());
+            } else if &line[..4] == "cbrt" {
+                let lists: Vec<_> = line.split(" ").collect();
+                println!("{}", lists[1].parse::<f64>().unwrap().cbrt());
             } else if line.contains("+") {
                 let re = Regex::new(r"(\d*) \+ (\d*)").unwrap();
                 for cap in re.captures_iter(&line) {
