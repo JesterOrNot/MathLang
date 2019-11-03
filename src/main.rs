e669c7e818f433a573e660658226dae473ce454e
Sean Hellum <seanhellum45@gmail.com>
2019-11-03 01:25:19 +0000

    edits

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate regex;
 use regex::Regex;
 use std::env;
-use std::io::{BufReader,BufRead};
 use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 
 fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
@@ -13,45 +13,57 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-fn main(){
+fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() == 2 {
         let lines = lines_from_file(&args[1]);
         for line in lines {
             if &line[..4] == "sqrt" {
-                println!("{}",f64::sqrt(line[5..].parse::<f64>().unwrap()));
-            }
-            else if line.contains("+") {
+                println!("{}", f64::sqrt(line[5..].parse::<f64>().unwrap()));
+            } else if line.contains("+") {
                 let re = Regex::new(r"(\d*) \+ (\d*)").unwrap();
                 for cap in re.captures_iter(&line) {
-                    println!("{}", &cap[1].parse::<f64>().unwrap() + &cap[2].parse::<f64>().unwrap());
+                    println!(
+                        "{}",
+                        &cap[1].parse::<f64>().unwrap() + &cap[2].parse::<f64>().unwrap()
+                    );
                 }
-            }
-            else if line.contains("-") {
+            } else if line.contains("-") {
                 let re = Regex::new(r"(\d*) \- (\d*)").unwrap();
                 for cap in re.captures_iter(&line) {
-                    println!("{}", &cap[1].parse::<f64>().unwrap() - &cap[2].parse::<f64>().unwrap());
+                    println!(
+                        "{}",
+                        &cap[1].parse::<f64>().unwrap() - &cap[2].parse::<f64>().unwrap()
+                    );
                 }
-            }
-            else if line.contains("*") && !line.contains("**") {
+            } else if line.contains("*") && !line.contains("**") {
                 let re = Regex::new(r"(\d*) \* (\d*)").unwrap();
                 for cap in re.captures_iter(&line) {
-                    println!("{}", &cap[1].parse::<f64>().unwrap() * &cap[2].parse::<f64>().unwrap());
+                    println!(
+                        "{}",
+                        &cap[1].parse::<f64>().unwrap() * &cap[2].parse::<f64>().unwrap()
+                    );
                 }
-            }
-            else if line.contains("/") {
+            } else if line.contains("/") {
                 let re = Regex::new(r"(\d*) / (\d*)").unwrap();
                 for cap in re.captures_iter(&line) {
-                    println!("{}", &cap[1].parse::<f64>().unwrap() / &cap[2].parse::<f64>().unwrap());
+                    println!(
+                        "{}",
+                        &cap[1].parse::<f64>().unwrap() / &cap[2].parse::<f64>().unwrap()
+                    );
                 }
-            }
-            else if line.contains("**") && !line.contains("**") {
+            } else if line.contains("**") && !line.contains(" * ") {
                 let re = Regex::new(r"(\d*) \*\* (\d*)").unwrap();
                 for cap in re.captures_iter(&line) {
-                    println!("{}", cap[1].parse::<f64>().unwrap().powf(cap[2].parse::<f64>().unwrap()));
+                    println!(
+                        "{}",
+                        cap[1]
+                            .parse::<f64>()
+                            .unwrap()
+                            .powf(cap[2].parse::<f64>().unwrap())
+                    );
                 }
-            }
-            else {
+            } else {
                 println!("Syntax Error!");
             }
         }
