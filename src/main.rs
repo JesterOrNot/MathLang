cc596854f109e5202315d72ecb5766fb5c62c617
Sean Hellum <seanhellum45@gmail.com>
2019-11-04 02:44:20 +0000

    format document

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,7 @@ use std::path::Path;
 pub fn lines_from_file(filename: impl AsRef<Path>) -> impl Iterator<Item = String> {
     let file = File::open(filename).expect("no such file");
     let buf = BufReader::new(file);
-    buf.lines()
-        .map(|l| l.expect("Could not parse line"))
+    buf.lines().map(|l| l.expect("Could not parse line"))
 }
 
 pub fn main() {
